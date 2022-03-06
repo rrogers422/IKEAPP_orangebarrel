c8557ed3f973b4b86c0712a814f7b793a693d649
rrogers422 <Reagan422440@gmail.com>
2022-03-06 11:32:12 -0500

    created connection to DB

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ require("dotenv").config();
 const app = express();
 //parse incoming req with json
 app.use(express.json());
+
+//connection to database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST, //localhost
+    user: process.env.DB_USER, //root
+    password: process.env.DB_PASSWORD, //password
+    databsae: process.env.DB, //orangebarrel
+});
